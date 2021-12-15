1199bee3c5fddb8a18d334dade7e034b97492b14
Uwe McVogt <mac@uv-software.de>
2021-12-15 18:48:56 +0100

    Update Xcode environment of the trial program
    Relates to [MACCAN-100]

--- Sources/Wrapper/can_api.cpp
+++ Sources/Wrapper/can_api.cpp
@@ -1,7 +1,7 @@
 //
 //  CAN Interface API, Version 3 (for Rusoku TouCAN USB Interfaces)
 //
-//  Copyright (C) 2020-2021  Uwe Vogt, UV Software, Berlin (info@mac-can.com)
+//  Copyright (c) 2020-2021  Uwe Vogt, UV Software, Berlin (info@mac-can.com)
 //
 //  This file is part of MacCAN-TouCAN.
 //
@@ -21,7 +21,7 @@
 #include "build_no.h"
 #define VERSION_MAJOR    0
 #define VERSION_MINOR    2
-#define VERSION_PATCH    2
+#define VERSION_PATCH    99
 #define VERSION_BUILD    BUILD_NO
 #define VERSION_STRING   TOSTRING(VERSION_MAJOR) "." TOSTRING(VERSION_MINOR) "." TOSTRING(VERSION_PATCH) " (" TOSTRING(BUILD_NO) ")"
 #if defined(_WIN64)
